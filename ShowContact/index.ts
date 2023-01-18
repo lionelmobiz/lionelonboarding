3378f5445b43c44a428b513b6704a7568e9b6d26
thabondlovuMobiz <66218398+thabondlovuMobiz@users.noreply.github.com>
2023-01-18 14:09:24 +0200

    Update index.ts

--- ShowContact/index.ts
+++ ShowContact/index.ts
@@ -1,10 +1,17 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+
+    // unnecessary comments
     context.log('HTTP trigger function processed a request.');
+    
+    // unused variable
     const id = (req.query.id || (req.body && req.body.id));
+    
     context.log('JavaScript queue trigger function processed work item');
     const contact = context.bindings.contact
+    
+    // is contact an array on and object?
     if (contact == undefined || contact.length == 0) {
         context.res = {
             status: 404, 
@@ -16,6 +23,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         context.log("Found contact item, Description=" + contact);
         context.res = {
             status: 200,
+            // what are you returnning as above we expect an object
             body: context.bindings.contact
         };
     }
