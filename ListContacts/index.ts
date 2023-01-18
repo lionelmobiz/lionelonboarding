0fcfdc41a58bb6c8805e83d122f47f25cfc026a9
thabondlovuMobiz <66218398+thabondlovuMobiz@users.noreply.github.com>
2023-01-18 14:14:37 +0200

    Update index.ts

--- ListContacts/index.ts
+++ ListContacts/index.ts
@@ -3,6 +3,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     
+    // result must be tenant specific
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: context.bindings.contactsList
