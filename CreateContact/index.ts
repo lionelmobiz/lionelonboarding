f34b43b2c24b45c44bc93e6f152fc00b42f8a874
Lionerdious <lionerdious@gmail.com>
2023-01-18 13:24:58 +0200

    create contact

---
+++ CreateContact/index.ts
@@ -0,0 +1,26 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+    
+    if (req.body) 
+    {
+        
+        context.bindings.contact = req.body;
+        context.res = {
+            status: 200, /* Defaults to 200 */
+            body: req.body
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Request body required."
+        };
+    }       
+  
+
+
+};
+
+export default httpTrigger;
